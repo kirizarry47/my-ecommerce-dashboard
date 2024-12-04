dbff1ca2c9d6c57035522b1040f004f144778c2c
Kurt Irizarry <kurtsmac@Kurts-MBP.attlocal.net>
2024-12-03 19:18:32 -0500

    started navbar need to work on hover effect for form button

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
 
-const Navbar = () => {
+import 'boxicons/css/boxicons.min.css';
+
+ const Navbar = () => {
+  const[isDarkmode, setIsDarkMode] =
+  useState(false);
+
+  useEffect(() => {
+    if(isDarkmode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [isDarkmode]);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkmode);
+  }; 
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="top-navbar">
+      <a href='#' className="nav-link">
+        Catergories
+      </a>
+      <form action="#">
+        <div className='form-input'>
+          <input type='search' placeholder='Search...' />
+          <button type='submit' className='search-btn'>
+            <i className='bx bx-search'></i>
+          </button>
+        </div>
+      </form>
 
-export default Navbar
+      <input type='checkbox' id='switch-mode' checked={isDarkmode} onChange={toggleDarkMode} hidden />
+
+      <label htmlFor='switch-mode' className='switch-mode'></label>
+
+      <a href='#' className='notification'>
+        <i className='bx bxs-bell'></i>
+        
+        <span className='num'>5</span>
+      </a>
+      
+      <a href='#' className='profile'>
+        <img src='https://media.geeksforgeeks.org/gfg-gg-logo.svg' alt='Profile' />
+      </a>
+    </nav>
+  )
+};
+
+export default Navbar;
